5cb3bc854a76234f1791a2c0763741a0619968b7
Andrii Zilnyk <andrii.zilnyk@intellias.com>
2023-10-29 20:42:26 +0200

    added readme.md, files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Graphql Apollo Server v4 Typescript
+
+### Supported types
+1. Scalar (Int, Float, String, Boolean, ID)
+2. Object (Root Operation Types)
+  * Query
+  * Mutation
+  * Subscription
+3. Input
+4. Enum
+5. Union
+6. Interface
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 
 const typeDefs = `#graphql
   type Book {
-    title: String
+    title: String!
+    description: String
+    isAvailable: Boolean!
+    price: Float
+    count: Int
     author: String
   }
 
   type Query {
-    books: [Book]
+    books: [Book!]!
   }
 `;
 
@@ -16,10 +20,18 @@ const books = [
   {
     title: 'The Awakening',
     author: 'Kate Chopin',
+    description: null,
+    isAvailable: true,
+    count: 8,
+    price: 24.98
   },
   {
     title: 'City of Glass',
     author: 'Paul Auster',
+    description: null,
+    isAvailable: true,
+    count: 12,
+    price: 32.17
   },
 ];
 
@@ -34,6 +46,12 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+(async () => {
+  try {
+    const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
-console.log(`🚀 Server listening at: ${url}`);
+    console.log(`🚀 Server listening at: ${url}`);
+  } catch (err ) {
+    console.log(err);
+  }
+})();
